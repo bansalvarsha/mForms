7cee9e5363db237d2ede1451f3fbf107e8511792
Varsha Bansal <vbansal@astegic.com>
2016-05-14 01:01:05 +0530

    Update TC007_AddResponse.java

--- src/test/java/rc25/mFormsAutomation/Testcases/TC007_AddResponse.java
+++ src/test/java/rc25/mFormsAutomation/Testcases/TC007_AddResponse.java
@@ -1,8 +1,3 @@
-/*---------------------------------------------------------------------------------------------------------------------
-<copyright file="TC007_AddResponse.java" company="Astegic Info solutions" Product= "MForms">
-Copyright 2016
-</copyright>
------------------------------------------------------------------------------------------------------------------------*/
 package rc25.mFormsAutomation.Testcases;
 
 import org.apache.log4j.xml.DOMConfigurator;
